1daec0ecb8c047ff337aac4bca7a23667b8f7fe8
ALEXEY VORONIN <linlongrus@gmail.com>
2025-03-28 12:17:11 -0400

    revorked the app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import pandas as pd
 import os
 app = Flask(__name__)
 DATA_FILE = "excerpts_database.tsv"
 if os.path.exists(DATA_FILE):
     df = pd.read_csv(DATA_FILE, sep="\t", encoding="cp1252")
+    df = df.fillna("N/A")
+    df.replace("", "N/A", inplace=True)
 else:
     df = pd.DataFrame()
 @app.route("/")
